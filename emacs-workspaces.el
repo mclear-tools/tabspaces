fd282c4d710903609fd872ba5606b6df622c4c06
Colin McLear <mclear@fastmail.com>
2022-02-14 17:56:12 -0500

    Add MIT license boilerplate

--- emacs-workspaces.el
+++ emacs-workspaces.el
@@ -18,6 +18,28 @@
 ;; project-based workspaces) and buffer isolation per tab. The package assumes
 ;; project.el and tab-bar.el are both present (they are built-in to emacs 28+).
 
+;; This file is not part of GNU Emacs.
+
+;; MIT License
+
+;; Permission is hereby granted, free of charge, to any person obtaining a copy
+;; of this software and associated documentation files (the "Software"), to deal
+;; in the Software without restriction, including without limitation the rights
+;; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+;; copies of the Software, and to permit persons to whom the Software is
+;; furnished to do so, subject to the following conditions:
+
+;; The above copyright notice and this permission notice shall be included in
+;; all copies or substantial portions of the Software.
+
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+;; SOFTWARE.
+
 ;; Much of the package code is from or is inspired by:
 
 ;; - https://github.com/kaz-yos/emacs
