c91858b4b5337459e60132549296acb0e3fce53c
Colin McLear <mclear@fastmail.com>
2022-03-25 10:37:13 -0400

    Create LICENSE
    - Use GPLv3 license & add boilerplate

--- emacs-workspaces.el
+++ emacs-workspaces.el
@@ -11,6 +11,17 @@
 ;; Keywords: workspaces, projects, tabs
 ;; URL: https://github.com/mclear-tools/emacs-workspaces
 
+;; This program is free software: you can redistribute it and/or modify it under
+;; the terms of the GNU General Public License as published by the Free Software
+;; Foundation, either version 3 of the License, or (at your option) any later
+;; version. This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+;; FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+;; details.
+
+;; You should have received a copy of the GNU General Public License along with
+;; this program. If not, see <https://www.gnu.org/licenses/>.
+
 ;;; Commentary:
 
 ;; This package provides several functions to facilitate a single frame-based
@@ -20,27 +31,7 @@
 
 ;; This file is not part of GNU Emacs.
 
-;; MIT License
-
-;; Permission is hereby granted, free of charge, to any person obtaining a copy
-;; of this software and associated documentation files (the "Software"), to deal
-;; in the Software without restriction, including without limitation the rights
-;; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-;; copies of the Software, and to permit persons to whom the Software is
-;; furnished to do so, subject to the following conditions:
-
-;; The above copyright notice and this permission notice shall be included in
-;; all copies or substantial portions of the Software.
-
-;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-;; SOFTWARE.
-
-;; Much of the package code is from or is inspired by:
+;; Much of the package code is inspired by:
 
 ;; - https://github.com/kaz-yos/emacs
 ;; - https://github.com/wamei/elscreen-separate-buffer-list/issues/8
